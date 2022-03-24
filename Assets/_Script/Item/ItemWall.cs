56f1e9a80991ba75206bb829656849ac685cbee9
Dogheadking <1714980277@qq.com>
2022-03-24 18:16:26 +0800

    v1.2

--- Assets/_Script/Item/ItemWall.cs
+++ Assets/_Script/Item/ItemWall.cs
@@ -8,17 +8,34 @@ public class ItemWall : ItemBase<Player>
     public GirlFollow Girl;
     [Range(0f, 1f)]
     public float AnimationScale;
+    public SkinnedMeshRenderer GirlShader;
+    private bool CanGo;
 
     public override void Init()
     {
         base.Init();
+        CanGo = false;
         Girl.Animator.speed = 0;
         UpdateAnimation();
     }
 
     public override void OnTargetEffect(Player target)
     {
+    }
 
+    public void Update()
+    {
+        var distance = Mathf.Abs(Player.Control.AnimationScale - AnimationScale);
+        if (distance <= 0.05f)
+        {
+            CanGo = true;
+            GirlShader.materials[0].SetColor("_OutlineColor", Color.green);
+        }
+        else
+        {
+            CanGo = false;
+            GirlShader.materials[0].SetColor("_OutlineColor", Color.red);
+        }
     }
 
     [Button("Test")]
